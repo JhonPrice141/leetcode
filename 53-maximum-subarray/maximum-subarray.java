6a00a50c76003bf8999f5925a098e91888ac1ffe
CaptainPrice <yogarajjuju222@gmail.com>
2025-10-03 11:14:50 +0530

    Time: 1 ms (99.45%) | Memory: 57.1 MB (43.33%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxsum =Integer.MIN_VALUE , cursum=0;
+        for(int i=0;i<nums.length;i++){
+            cursum+=nums[i];
+              maxsum = Math.max(maxsum,cursum);
+               if(cursum<0){
+            cursum=0;
+        }
+             
+        } 
+      
+       
+       
+        return maxsum;
+        
+    }
+}
